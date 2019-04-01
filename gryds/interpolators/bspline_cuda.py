e8112e32f3165b87bc5264448e8ec7ab9725bd6e
Koen Eppenhof <k.a.j.eppenhof@tue.nl>
2019-04-01 15:10:29 +0200

    Fixes reshaping cupy array

--- gryds/interpolators/bspline_cuda.py
+++ gryds/interpolators/bspline_cuda.py
@@ -71,16 +71,16 @@ class BSplineInterpolatorCUDA(Interpolator):
 
         # Reshape points for the cupy map_coordinates function to
         # receive coordinates in the expected shape
-        points = np.transpose(points.reshape(self.image.ndim, -1))
+        points_gpu = np.transpose(points.reshape(self.image.ndim, -1))
         sample_gpu = nd.map_coordinates(input=cp.array(self.image),
-                                        coordinates=cp.array(points),
+                                        coordinates=cp.array(points_gpu),
                                         mode=new_mode,
                                         order=new_order,
                                         cval=new_cval)
 
         # Convert back to CPU array and reshape to original shape
         sample_cpu = cp.asnumpy(sample_gpu)
-        sample = sample_cpu.reshape(self.image.shape)
+        sample = sample_cpu.reshape(points.shape)
         return np.array(sample.astype(DTYPE))
 
     def resample(self, grid, mode=None, order=None, cval=None):
